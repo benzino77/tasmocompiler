99a6794b80b40aa01e05399cf74d6a7158e8d357
Piotr Antczak <piotr@antczak.info>
2019-05-14 09:11:56 +0200

    Scrolling browser window to the bottom to make download links visible when compilation finished with success.

--- src/components/DownloadLinks/DownloadLinks.js
+++ src/components/DownloadLinks/DownloadLinks.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
-const DownloadLinks = (props) => {
-  const { classes } = props;
-  return (
-    <React.Fragment>
-      <Typography variant='caption' className={classes.link}>
-        You can now download custom compiled binary and files used during compilation:
-      </Typography>
-      <Typography>
-        <Link href={'/download/firmware.bin'} className={classes.link}>firmware.bin</Link>
-        <Link href={'/download/platformio.ini'} className={classes.link}>platformio.ini</Link>
-        <Link href={'/download/user_config_override.h'} className={classes.link}>user_config_override.h</Link>
-      </Typography>
-    </React.Fragment>
-  )
+class DownloadLinks extends Component {
+  constructor(props) {
+    super(props);
+    this.downloadLinksElement = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.downloadLinksElement.current.scrollIntoView({ block: 'end', inline: 'nearest', behavior: 'smooth' });
+    this.downloadLinksElement.current.scrollIntoView(false);
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div ref={this.downloadLinksElement}>
+        <Typography variant='caption' className={classes.link}>
+          You can now download custom compiled binary and files used during compilation:
+        </Typography>
+        <Typography>
+          <Link href={'/download/firmware.bin'} className={classes.link}>firmware.bin</Link>
+          <Link href={'/download/platformio.ini'} className={classes.link}>platformio.ini</Link>
+          <Link href={'/download/user_config_override.h'} className={classes.link}>user_config_override.h</Link>
+        </Typography>
+      </div>
+    )
+  }
 }
 
 export default DownloadLinks;
