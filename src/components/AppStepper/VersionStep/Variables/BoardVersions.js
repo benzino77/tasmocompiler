8a6a89863943184b132c5a0b5e6670572ce677be
Piotr Antczak <piotr@antczak.info>
2019-05-14 07:03:07 +0200

    Change board description from Mbit to MB

--- src/components/AppStepper/VersionStep/Variables/BoardVersions.js
+++ src/components/AppStepper/VersionStep/Variables/BoardVersions.js
@@ -1,16 +1,16 @@
 const boardVersions = [
   {
-    name: 'Sonoffs (1Mbit)',
+    name: 'Sonoffs (1MB)',
     // value: { mem_suffix: '.flash.1m0.ld', board: 'esp01_1m' },
     value: { mem: 1, board: 'esp01_1m', },
   },
   {
-    name: 'NodeMCU (4Mbit)',
+    name: 'NodeMCU (4MB)',
     // value: {mem_suffix: '.flash.4m1m.ld', board: 'nodemcuv2'},
     value: { mem: 4, board: 'nodemcuv2', },
   },
   {
-    name: 'Wemos D1 Mini (4Mbit)',
+    name: 'Wemos D1 Mini (4MB)',
     // value: {mem_suffix: '.flash.4m1m.ld', board: 'd1_mini'},
     value: { mem: 4, board: 'd1_mini', },
   },
