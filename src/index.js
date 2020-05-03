988c4cdbd1cec7bad68a03e22c5fbbe8fe251c78
Rapejim <rapejim@hotmail.com>
2020-05-03 04:13:10 +0200

    Implement react-intl library and browser language detection for locale translations

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'typeface-roboto';
+import { IntlProvider } from 'react-intl';
+import { allMessages } from './locales/languages';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log('Detecting current language of browser...');
+const currentLocale = navigator.language.split(/[-_]/)[0];
+console.log('Browser language is: ' + currentLocale);
+const defaultLocale = 'en';
+
+ReactDOM.render(
+    <IntlProvider locale={currentLocale} messages={allMessages[currentLocale]} defaultLocale={defaultLocale}>
+        <App />
+    </IntlProvider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
