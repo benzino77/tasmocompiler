f984d7ad831edbc92a94fa49357819f32564dff8
Rapejim <rapejim@hotmail.com>
2020-05-03 04:15:14 +0200

    Added empty translations files for EN, ES and IT languages

---
+++ src/locales/languages.js
@@ -0,0 +1,10 @@
+
+import localeEN from "../locales/en.json";
+import localeES from "../locales/es.json";
+import localeIT from "../locales/it.json";
+
+export const allMessages = {
+    'es': localeES,
+    'en': localeEN,
+    'it': localeIT,
+}
