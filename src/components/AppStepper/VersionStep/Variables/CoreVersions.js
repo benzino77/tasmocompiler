0b7bed633a991a72df9ea38dbe7dcf85c8c3531d
Piotr Antczak <antczak.piotr@gmail.com>
2020-12-16 17:56:02 +0100

    Compatibility changes and features added - remove Tasmota versions older than 8.5.x from the available versions - remove old and buggy Arduino Core versions from available options - add button to download gizpped version of compiled firmware

--- src/components/AppStepper/VersionStep/Variables/CoreVersions.js
+++ src/components/AppStepper/VersionStep/Variables/CoreVersions.js
@@ -1,12 +1,12 @@
 const coreVersions = [
-  {
-    name: '2.6.1',
-    value: { platform: 'core_2_6_1', mem_prefix: 'eagle.flash.' },
-  },
-  {
-    name: '2.7.1',
-    value: { platform: 'core_2_7_1', mem_prefix: 'eagle.flash.' },
-  },
+  // {
+  //   name: '2.6.1',
+  //   value: { platform: 'core_2_6_1', mem_prefix: 'eagle.flash.' },
+  // },
+  // {
+  //   name: '2.7.1',
+  //   value: { platform: 'core_2_7_1', mem_prefix: 'eagle.flash.' },
+  // },
   {
     name: '2.7.3',
     value: { platform: 'core_2_7_3', mem_prefix: 'eagle.flash.' },
