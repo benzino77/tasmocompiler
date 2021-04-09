775d0cd5b6441e1008f189e45339004606221c7b
drhideg <52567253+drhideg@users.noreply.github.com>
2021-04-09 18:02:32 +0200

    Fill missing translations from english

--- src/locales/languages.js
+++ src/locales/languages.js
@@ -5,7 +5,7 @@ import localePL from '../locales/pl.json';
 import localeCS from '../locales/cs.json';
 import localeHU from '../locales/hu.json';
 
-export const allMessages = {
+const allMessages = {
   en: localeEN,
   es: localeES,
   it: localeIT,
@@ -13,3 +13,30 @@ export const allMessages = {
   cs: localeCS,
   hu: localeHU,
 };
+
+function fillMessages(locale) {
+  const messages = allMessages[locale];
+  for (var key in allMessages['en']) {
+    if (allMessages['en'].hasOwnProperty(key)) {
+      if (Object.prototype.toString.call(allMessages['en'][key]) === '[object Object]') {
+        if (!messages.hasOwnProperty(key)) {
+          messages[key] = {};
+        }
+        fillMessages(allMessages['en'][key], messages[key]);
+      } else {
+        if (!messages.hasOwnProperty(key)) {
+          console.log('Missing translation: %s:%s', locale, key);
+          messages[key] = allMessages['en'][key];
+        }
+      }
+    }
+  }
+}
+
+for (const [locale] of Object.entries(allMessages)) {
+  if (locale !== 'en') {
+    fillMessages(locale);
+  }
+}
+
+export { allMessages };
