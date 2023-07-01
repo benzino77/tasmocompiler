568b9045dac9bc6b6ea4308d0765b285c7f06f12
Piotr Antczak <antczak.piotr@gmail.com>
2023-07-01 16:13:12 +0200

    refactor(jest): change jest configuration - this will prevent handlebars warnings  during `testbackend`

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   setupFiles: [path.resolve(__dirname, 'jestSetEnv.js')],
+  coverageReporters: ['text', 'text-summary'], // html reporter can be added to generate webpage with summary
 };
