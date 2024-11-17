83133a16edd20b192a8d62ddcb02b6333f79a3a6
Piotr Antczak <antczak.piotr@gmail.com>
2024-11-17 12:11:25 +0100

    chore: update theme colors

--- src/App.js
+++ src/App.js
@@ -40,16 +40,10 @@ console.log(`TasmoCompiler GUI language set to ${tcGUILanguage}`);
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
-      contrastText: '#fff',
+      main: '#3F51B5',
     },
     secondary: {
-      light: '#ff7961',
-      main: '#f44336',
-      dark: '#ba000d',
-      contrastText: '#000',
+      main: '#F50057',
     },
   },
 });
