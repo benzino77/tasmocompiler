16c84c5e3c248a3074c27aeb38176497a7854d44
ThomasCr <thomas.creutz@gmx.de>
2025-07-10 19:02:00 +0200

    Update AvailableBoards.js
    Add ESP32-S3 Board `opi_opi` (OPI Flash/OPI PSRAM) and updated the two other ESP32-S3 boards

--- src/components/AppStepper/FeaturesStep/AvailableBoards.js
+++ src/components/AppStepper/FeaturesStep/AvailableBoards.js
@@ -347,9 +347,31 @@ const availableBoards = [
   },
   // ESP32 S3
   {
-    name: 'esp32s3-qspi',
+    name: 'esp32s3-opi_opi',
     chip_type: 'esp32',
-    description: 'ESP32 S3 QSPI',
+    description: 'ESP32 S3 OPI Flash/OPI PSRAM',
+    default: false,
+    show: true,
+    platformio_entries: {
+      extends: 'env:tasmota32_base',
+      board: 'esp32s3-opi_opi',
+      // eslint-disable-next-line
+      build_flags: '${env:tasmota32_base.build_flags}',
+      // eslint-disable-next-line
+      lib_extra_dirs: ['${env:tasmota32_base.lib_extra_dirs}'],
+      // eslint-disable-next-line
+      lib_ignore: ['${env:tasmota32_base.lib_ignore}', 'Micro-RTSP', 'epdiy'],
+    },
+    platformio_env_name: 'tasmota32s3',
+    tooltip: '',
+    include_features: ['berry', 'ufilesys', 'rules'],
+    exclude_features: [],
+    defines: {},
+  },
+  {
+    name: 'esp32s3-qio_qspi',
+    chip_type: 'esp32',
+    description: 'ESP32 S3 QIO Flash/QSPI PSRAM',
     default: false,
     show: true,
     platformio_entries: {
@@ -369,9 +391,9 @@ const availableBoards = [
     defines: {},
   },
   {
-    name: 'esp32s3-opi',
+    name: 'esp32s3-qio_opi',
     chip_type: 'esp32',
-    description: 'ESP32 S3 OPI',
+    description: 'ESP32 S3 QIO Flash/OPI PSRAM',
     default: false,
     show: true,
     platformio_entries: {
